e64f526ef00665c3014dd8789461a7711d849842
anniebae <bae.annie@gmail.com>
2015-01-16 16:19:19 -0500

    backbone_todo

---
+++ backbone_todo/js/collections/todoCollection.js
@@ -0,0 +1,19 @@
+console.log("todoCollection");
+
+var TodoList = Backbone.Collection.extend({
+  model: Todo,
+  localStorage: new Backbone.LocalStorage("todos-backbone"),
+  done: function(){
+    return this.where({done: true});
+  },
+  remaining: function() {
+    return this.where({done: false});
+  },
+  nextOrder: function() {
+    if (!this.length) return 1;
+    return this.last().get('order') + 1;
+  },
+  comparator: 'order'
+});
+
+var Todos = new TodoList;
