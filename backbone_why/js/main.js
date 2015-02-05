d11efaebb4c2098b33ccc08766d055595e6f755d
anniebae <bae.annie@gmail.com>
2015-02-05 18:46:36 -0500

    added images and created models

---
+++ backbone_why/js/main.js
@@ -0,0 +1,42 @@
+modelOne = {
+  img: 'stylesheets/images/questionmark.jpg', 
+  caption: 'What is the meaning of life?', 
+  id: 1
+};
+modelTwo = {
+  img: 'stylesheets/images/school.jpg',
+  caption: 'Go to school to get educated',
+  id: 2
+};
+modelThree = {
+  img: 'stylesheets/images/diploma.jpg',
+  caption: 'Get an education to receive a diploma',
+  id: 3
+};
+modelFour = {
+  img: 'stylesheets/images/briefcase.jpg',
+  caption: 'Get a diploma to land a job',
+  id: 4
+};
+modelFive = {
+  img: 'stylesheets/images/money.jpg',
+  caption: 'Land a job to get paid',
+  id: 5
+};
+modelSix = {
+  img: 'stylesheets/images/bill.png',
+  caption: 'Get paid to pay the bills',
+  id: 6
+};
+modelSeven = {
+  img: 'stylesheets/images/lifestyle.jpg',
+  caption: 'Pay bills to support your lifestyle',
+  id: 7
+};
+modelEight = {
+  img: 'stylesheets/images/life.jpg',
+  caption: 'Have a lifestyle that defines your life',
+  id: 8
+};
+
+modelArray = [modelOne, modelTwo, modelThree, modelFour, modelFive, modelSix, modelSeven, modelEight]
