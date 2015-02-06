cc623e42adb27637abcbe321e0d90371cde8910a
anniebae <bae.annie@gmail.com>
2015-02-05 19:11:50 -0500

    moment_view implemented

---
+++ backbone_why/js/backbone_app/views/moment_view.js
@@ -0,0 +1,30 @@
+console.log("Moment view");
+
+var MomentView = Backbone.View.extend({
+  el: '#questionmark',
+  template: _.template($('#why-template').html()),
+  events: {
+    "click #dbl-exclimation" : "nextMoment",
+  },
+  initialize: function() {
+    this.render();
+  },
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  },
+  nextMoment: function() {
+    allMoments = momentsCollection.models;
+    counter = this.model.get('id');
+    console.log('counter', counter);
+    idOfNextModel = (counter + 1);
+      if (idOfNextModel > 8) {
+        idOfNextModel = 1;
+      }
+    newMoment = _.findWhere(allMoments, {id: idOfNextModel});
+    momentInTime = moment.set(newMoment.attributes);
+    new MomentView({
+      model: momentInTime
+    });
+  },
+});
